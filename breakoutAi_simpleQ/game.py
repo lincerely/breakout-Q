ab83c211d0ed1b5e248db88fb833128f7863f2e2
lincerely <lincerely@gmail.com>
2017-01-07 19:27:22 +0800

    Agent work perfectly with slow ball speed.
     On branch master
     Changes to be committed:
    
     	deleted:    breakoutAi/__init__.py
     	deleted:    breakoutAi/__init__.pyc
     	deleted:    breakoutAi/agent.py
     	deleted:    breakoutAi/breakoutenv.py
     	deleted:    breakoutAi/breakoutenv.pyc
     	deleted:    breakoutAi/breakouttask.py
     	deleted:    breakoutAi/breakouttask.pyc
     	deleted:    breakoutAi/trainedQ_breakout.npy
     	deleted:    breakoutAi/trainedQ_breakout_160r.npy
     	deleted:    breakoutAi/trainedQ_breakout_80r.npy
     	renamed:    breakoutAi/game.py -> breakoutAi_simpleQ/game.py
     	new file:   breakoutAi_simpleQ/trainedQ_breakout_10rs.npy
     	new file:   breakoutAi_simpleQ/trainedQ_breakout_10rs.npz

--- breakoutAi_simpleQ/game.py
+++ breakoutAi_simpleQ/game.py
@@ -5,7 +5,6 @@ import numpy as np
 import atexit
 import random
 
-
 black = [0, 0, 0]
 white = [255,255,255]
 grey = [180,180,180]
@@ -19,7 +18,7 @@ resolution = 10
 alpha = 0.5
 l = 0.9 #lambda
 
-Q = np.zeros((1280/resolution,480/resolution,3))
+
 
 STATES = {
     'Alive':0,
@@ -32,7 +31,7 @@ STATES = {
 ball_radius = 10
 paddle_width = 80
 paddle_height = 10
-
+#paddle_max_vec = 15
 
 
 class Brick():
@@ -47,10 +46,22 @@ class Brick():
 class Breakout(object):
 
 
-    def __init__(self):
+    def __init__(self,data):
+        self.highscore = 0
         self.isAuto = True
         self.command = 0
         self.iteration = 0
+        self.Q = np.zeros((1280/resolution,480/resolution,3))
+
+        if data is not None:
+            try:
+                print data.files
+                self.iteration = int(data['iter'])
+                self.highscore = int(data['high'])
+                self.Q = data['trainedQ']
+            except:
+                print("Can't load data from input file, wrong format.")
+                raise
 
         pygame.init()
 
@@ -62,15 +73,21 @@ class Breakout(object):
         self.screen = pygame.display.set_mode([640,480])
         self.myfont = pygame.font.SysFont("Arial",  30)
 
-    def update(self):
 
-        self.paddle_x += self.paddle_vec + self.com_vec
+
+
+    def update(self):
+        #if paddle_max_vec < abs(self.paddle_vec):
+        #    self.paddle_vec = paddle_max_vec * self.paddle_vec / abs(self.paddle_vec)
+
+        #self.paddle_x += self.paddle_vec
+
         if self.paddle_x < 0:
             self.paddle_x = 0
-            self.paddle_vec = 0
+            #self.paddle_vec = 0
         if self.paddle_x > self.screen.get_width() - paddle_width:
             self.paddle_x = self.screen.get_width() - paddle_width
-            self.paddle_vec = 0
+            #self.paddle_vec = 0
 
         self.current_reward = STATES['Alive']
         ##MOVE THE BALL
@@ -81,8 +98,9 @@ class Breakout(object):
 
     def randomAngle(self):
         self.ball_y = 450-ball_radius
-        self.ball_speed_x = random.randint(3,5) * self.ball_speed_x/abs(self.ball_speed_x)
-        self.ball_speed_y = random.randint(3,5) * self.ball_speed_y/abs(self.ball_speed_y)
+        ran = random.randint(0,4)
+        self.ball_speed_x = (7 - ran) * self.ball_speed_x/abs(self.ball_speed_x)
+        self.ball_speed_y = (3 + ran)* self.ball_speed_y/abs(self.ball_speed_y)
         self.ball_hit_count = 0
 
     def hitDetect(self):
@@ -95,18 +113,23 @@ class Breakout(object):
             self.current_reward = STATES['Dead']
             self.iteration+=1
             s = 'Iteration: '+repr(self.iteration) + ', max score: ' + repr(self.score) + ', hit count: '+repr(self.paddle_hit_count)
+
+            if self.score > self.highscore:
+                self.highscore = self.score
+                s += ' NEW HIGHSCORE!'
+
             print(s)
             self.resetGame()
 
         #for screen border
         if self.ball_y < ball_radius:
-            self.ball_y = 0
+            self.ball_y = ball_radius
             self.ball_speed_y = -self.ball_speed_y
         if self.ball_x < ball_radius:
-            self.ball_x = 0
+            self.ball_x = ball_radius
             self.ball_speed_x = -self.ball_speed_x
         if self.ball_x > self.screen.get_width() - ball_radius:
-            self.ball_x = self.screen.get_width()
+            self.ball_x = self.screen.get_width() - ball_radius
             self.ball_speed_x = -self.ball_speed_x
 
         #for paddle
@@ -150,10 +173,6 @@ class Breakout(object):
 
         if not self.isPressed:
             self.command = 0
-            #if self.paddle_vec >0:
-            #    self.paddle_vec -= self.paddle_speed
-            #elif self.paddle_vec < 0:
-            #    self.paddle_vec += self.paddle_speed
 
         return True
 
@@ -161,7 +180,7 @@ class Breakout(object):
         self.prev = [(self.ball_x-self.paddle_x+640)/resolution,self.ball_y/resolution]
 
         #Observe what state is in and perform the action that maximizes expected reward.
-        actions = Q[(self.ball_x-self.paddle_x+640)/resolution,self.ball_y/resolution,:]
+        actions = self.Q[(self.ball_x-self.paddle_x+640)/resolution,self.ball_y/resolution,:]
 
         maxs = [i for i,x in enumerate(actions) if x == np.argmax(actions)]
         if len(maxs) > 1:
@@ -177,43 +196,42 @@ class Breakout(object):
 
 
         if self.command == 1:
-            #self.com_vec -= self.paddle_speed
+            #self.paddle_vec -= self.paddle_speed
             self.paddle_x -= self.paddle_speed
         elif self.command == 2:
-            #self.com_vec += self.paddle_speed
+            #self.paddle_vec += self.paddle_speed
             self.paddle_x += self.paddle_speed
-        else:
-            if self.com_vec >0:
-                #self.com_vec -= self.paddle_speed
-                self.paddle_x -= self.paddle_speed
-            elif self.com_vec < 0:
-                #self.com_vec += self.paddle_speed
-                self.paddle_x += self.paddle_speed
+        #else:
+            #if self.paddle_vec >0:
+                #self.paddle_vec -= self.paddle_speed
+            #elif self.paddle_vec < 0:
+                #self.paddle_vec += self.paddle_speed
 
     def observe(self):
-        prev_Q = Q[self.prev[0],self.prev[1],self.command]
+        prev_Q = self.Q[self.prev[0],self.prev[1],self.command]
 
-        Q[self.prev[0],self.prev[1],self.command] = (
+        self.Q[self.prev[0],self.prev[1],self.command] = (
             prev_Q + alpha * (self.current_reward + l *
-                              max(Q[(self.ball_x-self.paddle_x+640)/resolution,self.ball_y/resolution,:])
+                              max(self.Q[(self.ball_x-self.paddle_x+640)/resolution,self.ball_y/resolution,:])
                               - prev_Q))
 
     def draw(self):
-        #DRAW EVERYTHING
-        #pygame.time.delay(1)
         self.screen.fill(white)
 
         score_label = self.myfont.render(str(self.score), 100, pygame.color.THECOLORS['black'])
         self.screen.blit(score_label, (5, 10))
+
+        count_label = self.myfont.render(str(self.paddle_hit_count), 100, pygame.color.THECOLORS['grey'])
+        self.screen.blit(count_label, (70, 10))
+
         if self.isAuto is True:
             auto_label = self.myfont.render("Auto", 100, pygame.color.THECOLORS['red'])
-            self.screen.blit(auto_label, (10, 10))
+            self.screen.blit(auto_label, (570, 10))
         for brick in self.bricks:
             pygame.draw.rect(self.screen,grey,brick.rect,0)
         pygame.draw.circle(self.screen, grey, [int(self.ball_x), int(self.ball_y)], ball_radius, 0)
         pygame.draw.rect(self.screen, grey, [self.paddle_x, self.paddle_y, paddle_width, paddle_height], 0)
 
-        #update the entire display
         pygame.display.update()
 
     def quit(self):
@@ -225,49 +243,62 @@ class Breakout(object):
             for j in range(1,5):
                 temp = Brick(70*i-35,50+20*j)
                 self.bricks.append(temp)
+
     def resetGame(self):
-                self.ball_x = 300
-                self.ball_y = 450-ball_radius
-                self.ball_speed_x = 3
-                self.ball_speed_y = 5
+        #pygame.time.wait(2000)
+        self.ball_x = 300
+        self.ball_y = 450-ball_radius
+        self.ball_speed_x = 3
+        self.ball_speed_y = 5
 
-                self.randomAngle()
+        self.randomAngle()
 
-                self.paddle_x = 300
-                self.paddle_y = 470
-                self.paddle_speed = 15
-                self.paddle_vec = 0
-                self.com_vec = 0
+        self.paddle_x = 300
+        self.paddle_y = 470
+        self.paddle_speed = 12
+        #self.paddle_vec = 0
+        self.com_vec = 0
 
-                self.score = 0
-                self.ball_hit_count = 0
-                self.paddle_hit_count = 0
+        self.score = 0
+        self.ball_hit_count = 0
+        self.paddle_hit_count = 0
 
-                self.initBricks()
+        self.initBricks()
+
+    def saveData(self):
+        data = [int(self.iteration), int(self.highscore),self.Q]
+        return data
+
 
 @atexit.register
 def save():
-    np.save(fname,Q)
-    print("Q saved successfully.")
+    savedata = game.saveData()
+    np.savez(fname,iter = savedata[0],high = savedata[1],trainedQ = savedata[2])
+    print("Data saved successfully.")
 
-
-game = Breakout()
 
 if len(sys.argv) > 1:
     try:
-        Q = np.load(str(sys.argv[1]))
+        data = np.load(str(sys.argv[1]))
+        game = Breakout(data)
         s = "Q loaded from " + str(sys.argv[1])+ " successfully."
         print(s)
+
     except IOError:
         s = "Error: can't find file or read data from " + str(sys.argv[1]) +", initializing a new Q matrix"
         print(s)
+        game = Breakout(None)
+
 else:
     try:
-        Q = np.load(fname + '.npy')
+        data = np.load(fname + '.npz')
+        game = Breakout(data)
         s = "Q loaded from " + str(fname)+ " successfully."
         print(s)
+
     except:
         print("Error on importing data, initializing a new Q matrix.")
+        game = Breakout(None)
 
 #game loop
 while game.input():
